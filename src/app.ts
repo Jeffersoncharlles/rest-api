04eceda549a959eee4ad857a6ea759b5c0906465
jeffersoncharlles <contato@jefferdeveloper.com>
2023-02-06 11:11:27 -0300

    chore:config knex db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
+
+app.get('/hello', async (req, res) => {
+  const test = await knex('sqlite_schema').select('*')
+
+  return test
+})
 
 app
   .listen({
