c3ea90b9a136b4e9418f050905797772bfc5c01f
jeffersoncharlles <contato@jefferdeveloper.com>
2023-02-10 00:53:01 -0300

    chore:concluid test e2e

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,6 +1,14 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync,spawnSync } from 'node:child_process'
 import { app } from '../bootstrap'
 import request from 'supertest'
+
+// Unitários: unidade da sua aplicação
+// Integração: comunicação entre duas ou mais unidades
+// e2e - ponta a ponta:simulam um usuário operando na nossa aplicação
+
+// Piramide de testes: E2E (nao dependem de nenhuma tecnologia, nao dependem de arquitetura )
+//   - 2000 testes -> Teste E2E => 16min
 
 describe('transactions routes', () => {
   beforeAll(async () => {
@@ -9,6 +17,10 @@ describe('transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
@@ -30,10 +42,7 @@ describe('transactions routes', () => {
         amount: 5000,
         type: 'credit',
       })
-
-
     const cookies = create.get('Set-cookie')
-
     const listAll = await request(app.server)
       .get('/transactions')
       .set('Cookie', cookies)
@@ -45,6 +54,57 @@ describe('transactions routes', () => {
         amount: 5000,
       })
     ])
+  })
+  it('should be able to get a specific transaction', async () => {
+   const create = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'example transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    const cookies = create.get('Set-cookie')
+    const listAll = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
 
+    const id = listAll.body.transactions[0].id
+
+    const getTransaction = await request(app.server)
+      .get(`/transactions/${id}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransaction.body.transactions).toEqual(
+      expect.objectContaining({
+        title: 'example transaction',
+        amount: 5000,
+      })
+    )
+  })
+  it('should be able to get the summary', async () => {
+    const create = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'example transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    const cookies = create.get('Set-cookie')
+    const createTwo = await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'debit example transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+    const summary = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summary.body.summary).toEqual({amount:3000})
   })
 })
